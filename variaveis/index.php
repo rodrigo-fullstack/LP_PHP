3dac1340f80c3008c12fba05ee7013f3ac38f48c
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2024-10-10 10:33:53 -0300

    feat (var): Variáveis em PHP

---
+++ variaveis/index.php
@@ -0,0 +1,19 @@
+<?php 
+    //Criação de variáveis
+    
+    //Iniciadas com cifrão
+    $var = 12;
+    $nome = "Rodrigo";
+    $diametro_bola = '15cm';
+
+    //Podem ser iniciados com sublinhados (underline)
+    $_nome = "Larissa";
+
+    //Case Sensitive
+    $Var = "Nova Variável";
+
+    echo "$var, $nome, $diametro_bola, $_nome, $Var";
+    
+
+
+?>
---
+++ variaveis/testando_nomes_obscuros.php
@@ -0,0 +1,12 @@
+<?php
+
+//Criando variáveis de mesmo nome através do cifrão
+$var = "Hello";
+$$var = 'world';
+
+//Exibindo variáveis
+echo "$var {$$var}";
+
+?>
+
+
